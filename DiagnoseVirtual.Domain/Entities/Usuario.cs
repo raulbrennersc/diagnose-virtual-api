29a9377f6b083843209c8029d10c1f67773f9a54
Raul Costa <raulbrennersc@gmail.com>
2020-01-19 21:47:37 -0300

    Domain

---
+++ DiagnoseVirtual.Domain/Entities/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace DiagnoseVirtual.Domain.Entities
+{
+    public class Usuario : BaseEntity
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
---
+++ DiagnoseVirtual.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using DiagnoseVirtual.Domain.Entities;
+using System.Collections.Generic;
+
+namespace DiagnoseVirtual.Domain.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        void Insert(T obj);
+
+        void Update(T obj);
+
+        void Remove(int id);
+
+        T Select(int id);
+
+        IList<T> SelectAll();
+    }
+}
