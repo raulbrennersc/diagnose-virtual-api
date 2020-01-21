aad45808119ed039e3d6aeb781c31241fc1224cd
Raul Costa <raulbrennersc@gmail.com>
2020-01-21 18:03:12 -0300

    Criando service para abrigar metodos especificos para o cadastro de usuarios

--- DiagnoseVirtual.Application/Controllers/AutenticacaoController.cs
+++ DiagnoseVirtual.Application/Controllers/AutenticacaoController.cs
@@ -1,6 +1,5 @@
 ﻿using DiagnoseVirtual.Domain.Dtos;
 using DiagnoseVirtual.Domain.Entities;
-using DiagnoseVirtual.Service.Business;
 using DiagnoseVirtual.Service.Helpers;
 using DiagnoseVirtual.Service.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -12,7 +11,7 @@ namespace DiagnoseVirtual.Application.Controllers
     [ApiController]
     public class AutenticacaoController : ControllerBase
     {
-        private readonly BaseService<Usuario> sUsuario = new BaseService<Usuario>();
+        private readonly UsuarioService sUsuario = new UsuarioService();
         private readonly IConfiguration config;
 
         public AutenticacaoController(IConfiguration config)
@@ -23,11 +22,10 @@ namespace DiagnoseVirtual.Application.Controllers
         [HttpPost("Register")]
         public ActionResult Register(UsuarioRegistroDto novoUsuarioDto)
         {
-            var bUsuario = new BUsuario(sUsuario);
-            if (bUsuario.ExisteUsuario(novoUsuarioDto.Cpf))
+            if (sUsuario.ExisteUsuario(novoUsuarioDto.Cpf))
                 return BadRequest("Já existe um cadastro com este CPF");
 
-            var novoUsuario = bUsuario.Cadastrar(novoUsuarioDto);
+            var novoUsuario = sUsuario.Cadastrar(novoUsuarioDto);
 
             return StatusCode(201);
         }
@@ -35,8 +33,7 @@ namespace DiagnoseVirtual.Application.Controllers
         [HttpPost("Login")]
         public ActionResult Login(UsuarioLoginDto usuarioLoginDto)
         {
-            var bUsuario = new BUsuario(sUsuario);
-            var usuario = bUsuario.Login(usuarioLoginDto.Cpf, usuarioLoginDto.Password);
+            var usuario = sUsuario.Login(usuarioLoginDto.Cpf, usuarioLoginDto.Password);
 
             if (usuario == null)
                 return Unauthorized();
