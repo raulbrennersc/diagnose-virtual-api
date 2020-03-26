f26c810aaa98b0d96b3625a763ec64d5b10846aa
Raul Costa <raulbrennersc@gmail.com>
2020-03-25 22:17:07 -0300

    Migration 8

---
+++ DiagnoseVirtual.Infra.Data/Migrations/20200326011634_8.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DiagnoseVirtual.Infra.Data.Migrations
+{
+    public partial class _8 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "contato",
+                schema: "diagnose_virtual",
+                table: "localizacao_fazenda");
+
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                schema: "diagnose_virtual",
+                table: "localizacao_fazenda",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "telefone",
+                schema: "diagnose_virtual",
+                table: "localizacao_fazenda",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                schema: "diagnose_virtual",
+                table: "localizacao_fazenda");
+
+            migrationBuilder.DropColumn(
+                name: "telefone",
+                schema: "diagnose_virtual",
+                table: "localizacao_fazenda");
+
+            migrationBuilder.AddColumn<string>(
+                name: "contato",
+                schema: "diagnose_virtual",
+                table: "localizacao_fazenda",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
