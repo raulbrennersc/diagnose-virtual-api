ca0f5d9529cedf151417c1f181bdbb2e07a2d0b2
Raul Costa <raulbrennersc@gmail.com>
2020-03-28 21:37:24 -0300

    Adicionando o swagger

--- DiagnoseVirtual.Application/Startup.cs
+++ DiagnoseVirtual.Application/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
 using System.Net;
 using System.Text;
 
@@ -25,6 +27,17 @@ namespace DiagnoseVirtual.Application
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers().AddNewtonsoftJson();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "DiagnoseVirtual API",
+                    Description = "ASP.NET Core Web API",
+                    TermsOfService = new Uri("http://www.sistema.qipixel.com.br/"),
+                });
+            });
+
             services.AddCors();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
@@ -69,6 +82,12 @@ namespace DiagnoseVirtual.Application
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseRouting();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DiagnoseVirtual");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseAuthorization();
 
