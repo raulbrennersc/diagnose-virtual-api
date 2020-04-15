61dc981313bd19bba6cbed66bf696603162d4766
Raul Costa <raulbrennersc@gmail.com>
2020-04-15 20:57:14 -0300

    Adicionando mapemento da cultura

---
+++ DiagnoseVirtual.Infra.Data/Mapping/CulturaMap.cs
@@ -0,0 +1,29 @@
+﻿using DiagnoseVirtual.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiagnoseVirtual.Infra.Data.Mapping
+{
+    public class CulturaMap : IEntityTypeConfiguration<Cultura>
+    {
+        public void Configure(EntityTypeBuilder<Cultura> builder)
+        {
+            //Tabela
+            builder.ToTable("cultura");
+            builder.HasKey(x => x.Id);
+
+            //Proriedades
+            builder.Property(x => x.Nome)
+                .HasColumnName("nome")
+                .IsRequired();
+
+            //Relacoes
+
+            //Indices
+            builder.HasIndex(x => x.Id).IsUnique();
+        }
+    }
+}
