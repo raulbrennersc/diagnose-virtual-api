7bbb913cc5dac923df8e353f2eb0c7e1fc235e8b
Raul Costa <raulbrennersc@gmail.com>
2020-04-15 20:26:28 -0300

    Adicionando nova migration

---
+++ DiagnoseVirtual.Infra.Data/Migrations/20200415232609_10.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DiagnoseVirtual.Infra.Data.Migrations
+{
+    public partial class _10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ativo",
+                schema: "diagnose_virtual",
+                table: "monitoramento",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ativo",
+                schema: "diagnose_virtual",
+                table: "monitoramento");
+        }
+    }
+}
