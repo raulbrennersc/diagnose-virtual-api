8e8e96c3372030ff207ca5a1d19a2a303c8cae51
Raul Costa <raulbrennersc@gmail.com>
2020-06-09 20:53:46 -0300

    Salvando id do pdi de talhoes

---
+++ DiagnoseVirtual.Infra.Data/Migrations/20200609235300_8.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DiagnoseVirtual.Infra.Data.Migrations
+{
+    public partial class _8 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "id_pdi",
+                schema: "diagnose_virtual",
+                table: "lavoura",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "id_pdi",
+                schema: "diagnose_virtual",
+                table: "lavoura");
+        }
+    }
+}
